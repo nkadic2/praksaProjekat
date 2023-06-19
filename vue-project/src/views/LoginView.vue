8e452a39ab002849450d2935b7f9bfa986f9e2f2
nkadic2 <nkadic2@etf.unsa.ba>
2023-06-19 09:33:44 +0200

    forma za registraciju

---
+++ vue-project/src/views/LoginView.vue
@@ -0,0 +1,93 @@
+<script>
+export default {
+    data() {
+    return {
+      loginEmail: '',
+      loginPassword: '',
+      isValidEmail: true,
+      isValidPassword: true,
+    };
+  },
+  name:"LoginView",
+
+  methods: {
+    submitForm() {
+      this.isValidEmail = this.validateEmail();
+      this.isValidPassword = this.validatePassword();
+
+      if (this.isValidEmail && this.isValidPassword) {
+        // Proceed with login logic or API call
+        console.log('Valid form');
+      }
+    },
+    validateEmail() {
+      // Basic email validation
+      const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      return regex.test(this.loginEmail);
+    },
+    validatePassword() {
+      //const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).+$/;
+      // Password should be at least 8 characters long
+      return this.loginPassword.length >= 8;
+    },
+  },
+}
+</script>
+
+<template>
+  <div class="flex justify-center">
+    <div class="w-full max-w-md">
+    <form @submit.prevent="submitForm"
+      action=""
+      class=" mb-0 mt-6 space-y-4 rounded-lg p-4 shadow-lg sm:p-6 lg:p-8">
+      <p class="text-center text-lg font-medium">Sign in to your account</p>
+      <div class="mb-4">
+        <label class="sr-only" for="email">
+          Email
+        </label>
+        <input
+          v-model="loginEmail"
+          :class="{ 'border-red-500': !isValidEmail }"
+          type="email"
+          id="email"
+          class="w-full rounded-lg p-4 pe-12 text-sm shadow-sms border-2 border-slate-400"
+          placeholder="Enter email"
+        />
+        <span v-if="!isValidEmail" class="text-red-500 text-xs italic">
+          Please enter a valid email address.
+        </span>
+      </div>
+      <div class="mb-4">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+        </label>
+        <input
+          v-model="loginPassword"
+          :class="{ 'border-red-500': !isValidPassword }"
+          type="password"
+          id="password"
+          class="w-full rounded-lg p-4 pe-12 text-sm shadow-sm border-2 border-slate-400"
+          placeholder="Enter password"
+        />
+        <span v-if="!isValidPassword" class="text-red-500 text-xs italic">
+          Password should be at least 8 characters long.
+        </span>
+      </div>
+      <button
+        type="submit"
+        class="block w-full rounded-lg bg-indigo-600 px-5 py-3 text-sm font-medium text-white"
+      >
+        Sign in
+      </button>
+
+      <p class="text-center text-sm text-gray-500">
+        No account?
+        <a class="underline" href="">Sign up</a>
+      </p>
+    </form>
+  </div>
+  </div>
+  
+</template>
+
+<style>
+</style>
---
+++ vue-project/src/views/RegistrationView.vue
@@ -0,0 +1,129 @@
+<script>
+//import { required, email, minLength, sameAs } from '@vuelidate/validators'
+//import { required, email, minLength, sameAs } from 'vuelidate/lib/validators';
+//import { VuelidatePlugin } from '@vuelidate/core';
+export default {
+  name:"RegistrationView",
+    /*data() {
+        return {
+            user: {
+                firstName: "",
+                lastName: "",
+                email: "",
+                password: "",
+                confirmPassword: ""
+            },
+            submitted: false
+        };
+    },
+    validations: {
+        user: {
+            firstName: { required },
+            lastName: { required },
+            email: { required, email },
+            password: { required, minLength: minLength(8) },
+            confirmPassword: { required, sameAsPassword: sameAs('password') }
+        }
+    },
+    methods: {
+        handleSubmit(e) {
+            this.submitted = true;
+            // stop here if form is invalid
+            this.$v.$touch();
+            if (this.$v.$invalid) {
+                return;
+            }
+
+            alert("SUCCESS!! :-)\n\n" + JSON.stringify(this.user));
+        }
+    }*/
+}
+</script>
+
+<template>
+   <div class="flex jutify-centers items-center w-screen h-screen">
+   <div class="w-full h-full max-w-xxxl">
+    <div class="lg:grid lg:min-h-screen lg:grid-cols-12">
+      <div class="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6" >
+          <form class=" mb-0 mt-6 space-y-4 rounded-lg p-4 shadow-lg sm:p-6 lg:p-8">
+            <p class="text-center text-lg font-medium">Registration</p>
+            <div class="col-span-6 sm:col-span-3" >
+              <label
+                for="FirstName"
+                class="block text-sm font-medium text-gray-700"
+              >
+              </label>
+              <input
+                type="text"
+                placeholder="First name"
+
+                class="w-full rounded-lg p-4 pe-12 text-sm shadow-sms border-2 border-slate-400"
+              />
+            </div>
+            <div class="col-span-6 sm:col-span-3">
+              <label
+                for="LastName"
+                class="block text-sm font-medium text-gray-700"
+              >
+              </label>
+              <input
+                type="text"
+                placeholder="Last name"
+                class="w-full rounded-lg p-4 pe-12 text-sm shadow-sms border-2 border-slate-400"
+              />
+            </div>
+            <div class="col-span-6">
+              <label for="Email" class="block text-sm font-medium text-gray-700">
+              </label>
+              <input
+                type="email"
+                placeholder="Email"
+                class="w-full rounded-lg p-4 pe-12 text-sm shadow-sms border-2 border-slate-400"
+              />
+
+            </div>
+            <div class="col-span-6 sm:col-span-3">
+              <label
+                for="Password"
+                class="block text-sm font-medium text-gray-700"
+              >
+              </label>
+              <input
+                type="password"
+                placeholder="Password"
+
+                class="w-full rounded-lg p-4 pe-12 text-sm shadow-sms border-2 border-slate-400"
+                />
+            </div>
+            <div class="col-span-6 sm:col-span-3">
+              <label
+                for="PasswordConfirmation"
+                class="block text-sm font-medium text-gray-700"
+              >
+              </label>
+              <input
+                type="password"
+                placeholder="Password Confirmation"
+
+                class="w-full rounded-lg p-4 pe-12 text-sm shadow-sms border-2 border-slate-400"/>
+                         
+            </div>
+            <div class="col-span-6 sm:flex sm:items-center sm:gap-4">
+              <button
+                class="block w-full rounded-lg bg-indigo-600 px-5 py-3 text-sm font-medium text-white"
+              >
+                Create an account
+              </button>
+              <p class="mt-4 text-sm text-gray-500 sm:mt-0">
+                Already have an account?
+                <router-link to="/login" class="text-gray-700 underline">Log in</router-link>
+              </p>
+            </div>
+          </form>
+        </div>
+    </div>  
+  </div>    
+</div>
+</template>
+<style>
+</style>
